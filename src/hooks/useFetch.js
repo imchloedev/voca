5ef779e1cbe9912297e0c6e48c39b7dbe08bbb84
Eunji Kang <etothej@naver.com>
2022-07-13 11:40:45 +0900

    useFetch added

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }, [url]);
+
+  return data;
+}
+
+
+//커스텀 훅 만들기
